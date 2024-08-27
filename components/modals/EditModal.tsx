3ea533e558ba20f624fe955a937f26956b6f1853
oluwamuibiclear <igbokweudoka@gmail.com>
2024-08-26 19:14:53 -0500

    user profile image upload component

--- components/modals/EditModal.tsx
+++ components/modals/EditModal.tsx
@@ -9,13 +9,14 @@ import { signIn } from "next-auth/react";
 import useCurrentUser from "@/hooks/useCurrentUser";
 import useUser from "@/hooks/useUser";
 import useEditModal from "@/hooks/useEditModal";
+import { ImageUpload } from "../ImageUpload";
 
 const EditModal = () => {
   const { data: currentUser } = useCurrentUser();
 
   const { mutate: mutateFetchedUser } = useUser(currentUser?.id);
   const editModal = useEditModal();
- 
+
   const [profileImage, setProfileImage] = useState("");
   const [coverImage, setCoverImage] = useState("");
   const [name, setName] = useState("");
@@ -28,98 +29,77 @@ const EditModal = () => {
     setName(currentUser?.name);
     setUsername(currentUser?.username);
     setBio(currentUser?.bio);
-
-    
   }, [currentUser]);
   const [isLoading, setIsLoading] = useState(false);
-    const onSubmit = useCallback(async () => {
-      try {
-        setIsLoading(true);
-       
-            await axios.patch("/api/edit", {
-                userId :currentUser?.id,
-                name,
-                username,
-                bio,
-                profileImage,
-                coverImage,
-              });
-              mutateFetchedUser();
-        toast.success("Updated");
-        editModal.onClose();
-        
-        
-        
-      } catch (error) {
-        console.log('error',error)
-        toast.error("Something went wrong");
-      } finally {
-        setIsLoading(false);
-      }
-    }, [
-      editModal,
-      name,
-      username,
-      bio,
-      profileImage,
-      coverImage,
-      mutateFetchedUser,
-    ]);
-    const bodyContent = (
-      <div className="flex flex-col gap-4">
-        
-        <Input
-          placeholder="Name"
-          onChange={(e) => setName(e.target.value)}
-          value={name}
-          disabled={isLoading}
-          
-        />
-        <Input
-          placeholder="Username"
-          onChange={(e) => setUsername(e.target.value)}
-          value={username}
-          disabled={isLoading}
-        />
-        <Input
-          placeholder="Bio"
-          onChange={(e) => setBio(e.target.value)}
-          value={bio}
-          disabled={isLoading}
-        />
-        {/* <Input
-          placeholder="Bio"
-          onChange={(e) => setBio(e.target.value)}
-          value={bio}
-          disabled={isLoading}
-          type="file"
-        /> */}
-        {/* <Input
-          placeholder="Password"
-          onChange={(e) => setpassword(e.target.value)}
-          value={password}
-          disabled={isLoading}
-          type="password"
-        /> */}
-      </div>
-    );
+  const onSubmit = useCallback(async () => {
+    try {
+      setIsLoading(true);
 
-  //   const footerContent = (
-  //     <div className="text-neutral-400 text-center mt-4">
-  //       <p>
-  //         Already have an account?
-  //         <span
-  //           onClick={}
-  //           className="text-white cursor-pointer hover:underline"
-  //         >
-  //           {" "}
-  //           Sign in
-  //         </span>
-  //       </p>
-  //     </div>
-  //   );
+      await axios.patch("/api/edit", {
+        userId: currentUser?.id,
+        name,
+        username,
+        bio,
+        profileImage,
+        coverImage,
+      });
+      mutateFetchedUser();
+      toast.success("Updated");
+      editModal.onClose();
+    } catch (error) {
+      console.log("error", error);
+      toast.error("Something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  }, [
+    editModal,
+    name,
+    username,
+    bio,
+    profileImage,
+    coverImage,
+    mutateFetchedUser,
+  ]);
+  const bodyContent = (
+    <div className="flex flex-col gap-4">
+
+    <ImageUpload
+        label="Upload profile image"
+        onChange={(image) => setProfileImage(image)}
+        value={profileImage}
+        disabled={isLoading}
+      />
+
+     <ImageUpload
+        label="Upload cover image"
+        onChange={(image) => setCoverImage(image)}
+        value={coverImage}
+        disabled={isLoading}
+      />
+      <Input
+        placeholder="Name"
+        onChange={(e) => setName(e.target.value)}
+        value={name}
+        disabled={isLoading}
+      />
+      <Input
+        placeholder="Username"
+        onChange={(e) => setUsername(e.target.value)}
+        value={username}
+        disabled={isLoading}
+      />
+      <Input
+        placeholder="Bio"
+        onChange={(e) => setBio(e.target.value)}
+        value={bio}
+        disabled={isLoading}
+      />
+
+    </div>
+  );
+
   return (
-  
     <Modal
       disabled={isLoading}
       isOpen={editModal.isOpen}
@@ -128,7 +108,7 @@ const EditModal = () => {
       onClose={editModal.onClose}
       onSubmit={onSubmit}
       body={bodyContent}
-    //   footer={footerContent}
+      //   footer={footerContent}
     />
   );
 };
